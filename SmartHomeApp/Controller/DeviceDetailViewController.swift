1ac4493d9f392ddd5e76bafce0382f4000456a84
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-07-13 08:14:28 +0300

    Create DeviceDetailViewController.swift

---
+++ SmartHomeApp/Controller/DeviceDetailViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+protocol DeviceDetailDelegate {
+
+    func onValueChanged(value: Float)
+    func onClickSwitch(isOn: Bool)
+    func onTapArrowUp()
+    func onTapArrowDown()
+}
+
+class DeviceDetailViewController: UIViewController {
+    
+    var viewModel: DeviceDetailViewModel!
+
+    init(device: Any) {
+        super.init(nibName: nil, bundle: nil)
+        setupView(for: device)
+        initViewModel(for: device)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(for device: Any) {
+        let v = DeviceDetailView(frame: view.frame, device: device)
+        v.delegate = self
+        view = v
+    }
+    
+    func initViewModel(for device: Any) {
+        viewModel = DeviceDetailViewModel(device: device)
+        if let view = self.view as? DeviceDetailView {
+            view.setNameText(text: viewModel.name)
+            view.setValueText(text: viewModel.valueText)
+            view.setColorViewHidden(isHidden: viewModel.isHidden)
+            view.setSwitchButton(isOn: viewModel.isOn)
+            view.setSliderValue(value: viewModel.value)
+            view.setColorViewColor(color: viewModel.color)
+            view.setColorViewHeight(height: viewModel.getColorViewHeight(with: view.frame.height))
+        }
+    }
+}
+
+extension DeviceDetailViewController: DeviceDetailDelegate {
+        
+    func onValueChanged(value: Float) {
+        viewModel.onValueChanged(value: value)
+        (view as? DeviceDetailView)?.setColorViewHeight(height: viewModel.getColorViewHeight(with: view.frame.height))
+        (view as? DeviceDetailView)?.setValueText(text: viewModel.valueText)
+    }
+
+    func onClickSwitch(isOn: Bool) {
+        viewModel.onClickSwitch(isOn: isOn)
+        (view as? DeviceDetailView)?.setColorViewHidden(isHidden: viewModel.isHidden)
+        (view as? DeviceDetailView)?.setValueText(text: viewModel.valueText)
+    }
+
+    func onTapArrowUp() {
+        viewModel.onTapArrowUp()
+        (view as? DeviceDetailView)?.setColorViewHeight(height: viewModel.getColorViewHeight(with: view.frame.height))
+        (view as? DeviceDetailView)?.setValueText(text: viewModel.valueText)
+    }
+
+    func onTapArrowDown() {
+        viewModel.onTapArrowDown()
+        (view as? DeviceDetailView)?.setColorViewHeight(height: viewModel.getColorViewHeight(with: view.frame.height))
+        (view as? DeviceDetailView)?.setValueText(text: viewModel.valueText)
+    }
+}
