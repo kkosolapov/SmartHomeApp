a098f934b60e4dad04f43dc04514684f3482e9ab
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-07-13 08:46:24 +0300

    Create SmartHomeAppUnitTests.swift

---
+++ SmartHomeAppUnitTests/SmartHomeAppUnitTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+
+class ModuloTechUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        continueAfterFailure = false
+
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+
+      let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
